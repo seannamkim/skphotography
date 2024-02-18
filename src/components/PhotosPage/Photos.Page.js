ef73a58838ded07b39eecfcf86babeb2d4282e96
Seanna Kim <seannakim@me.com>
2024-02-18 11:36:04 -0800

    scale for mobile

--- src/components/PhotosPage/Photos.Page.js
+++ src/components/PhotosPage/Photos.Page.js
@@ -86,6 +86,59 @@ export const PhotosPageWrap = styled.div`
       }
     }
   }
+
+  @media (max-width: 768px) {
+    .PhotosPage {
+      min-height: 100vh;
+      margin-top: 5rem;
+
+      & > .all-photos {
+        display: flex;
+        justify-content: space-between;
+        margin: 6rem 0;
+        flex-wrap: wrap;
+
+        & > .photos-column {
+          margin: 0 2rem;
+          display: flex;
+          flex-direction: column;
+          overflow: auto;
+
+          & > .location-link {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin: 1rem;
+            cursor: pointer;
+            transition: 350ms ease-in-out;
+
+            & > h2 {
+              position: absolute;
+              color: white;
+              animation: 500ms openphoto;
+              z-index: 1;
+              font-size: 2rem;
+              letter-spacing: 0.2rem;
+            }
+
+            & > .photo {
+              width: 20rem;
+              height: 20rem;
+              cursor: pointer;
+              z-index: 0;
+              object-fit: cover;
+              animation: 500ms openphoto;
+            }
+
+            &:hover {
+              text-shadow: 5px 5px 5px rgba(0, 0, 0, 0.5);
+              transition: 350ms ease-in-out;
+            }
+          }
+        }
+      }
+    }
+  }
 `;
 
 export default PhotosPageWrap;
--- src/components/PhotosSecondaryPage/Photos.Secondary.Page.js
+++ src/components/PhotosSecondaryPage/Photos.Secondary.Page.js
@@ -29,6 +29,36 @@ export const PhotosSecondaryPageWrap = styled.div`
       }
     }
   }
+  @media (max-width: 768px) {
+    .PhotosSecondaryPage {
+      min-height: 100vh;
+      margin-top: 5rem;
+
+      & > .location-title {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 3rem;
+        letter-spacing: 0.2rem;
+        padding-top: 1rem;
+      }
+
+      & > .all-photos-secondary {
+        display: flex;
+        justify-content: space-evenly;
+        flex-wrap: wrap;
+        width: 100%;
+
+        & > .photo-secondary {
+          margin: 2rem;
+          max-width: 80%;
+          height: min-content;
+          object-fit: cover;
+          cursor: pointer;
+        }
+      }
+    }
+  }
 `;
 
 export default PhotosSecondaryPageWrap;
