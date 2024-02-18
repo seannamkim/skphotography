decb39ea1d717805cd1470b9795b5cea9c1f331c
Seanna Kim <seannakim@me.com>
2024-02-18 11:38:28 -0800

    scale nav bar

--- src/components/NavBar/Nav.Bar.js
+++ src/components/NavBar/Nav.Bar.js
@@ -120,6 +120,103 @@ export const NavBarWrap = styled.div`
       }
     }
   }
+
+  @media (max-width: 768px) {
+    .NavBar {
+      display: flex;
+      justify-content: space-between;
+      position: fixed;
+      background-color: rgb(222, 222, 222);
+      top: 0;
+      width: 100%;
+      height: 5rem;
+      z-index: 2;
+
+      & > h1 {
+        display: flex;
+        align-items: center;
+        margin: 1rem;
+        letter-spacing: 0.2rem;
+        white-space: nowrap;
+        font-size: 1rem;
+      }
+
+      & > .nav-links {
+        display: flex;
+        align-items: center;
+        margin-right: 0.5rem;
+        font-size: 0.8rem;
+
+        & > .dropdown-arrow-right {
+          margin-right: 0.5rem;
+          margin-left: -0.5rem;
+          cursor: pointer;
+        }
+
+        & > .dropdown-arrow-down {
+          margin-right: 0.5rem;
+          margin-left: -0.5rem;
+          width: 8px;
+          cursor: pointer;
+        }
+
+        & > .photos-dropdown {
+          position: absolute;
+          top: 50px;
+          right: 25px;
+          background-color: rgb(200, 200, 200);
+          z-index: 2;
+          animation: 0.5s slidemenu;
+
+          & > .dropdown-items {
+            display: flex;
+            flex-direction: column;
+            animation: 1s slideitems;
+
+            & > .dropdown-item {
+              padding: 0.5rem;
+              color: grey;
+              text-decoration: none;
+
+              &:hover {
+                color: black;
+                background-color: rgba(0, 0, 0, 0.5);
+              }
+            }
+          }
+        }
+
+        & > .nav-link {
+          text-decoration: none;
+          margin-right: 0.5rem;
+          color: black;
+          letter-spacing: 0.2rem;
+          white-space: nowrap;
+        }
+
+        & > .selected {
+          text-decoration: none;
+          margin-right: 0.5rem;
+          color: grey;
+          letter-spacing: 0.2rem;
+          white-space: nowrap;
+
+          & > .dropdown-arrow-right {
+            margin-right: 0.5rem;
+            margin-left: -0.5rem;
+            cursor: pointer;
+          }
+
+          & > .dropdown-arrow-down {
+            margin-right: 0.5rem;
+            margin-left: -0.5rem;
+            width: 8px;
+            cursor: pointer;
+          }
+        }
+      }
+    }
+  }
 `;
 
 export default NavBarWrap;
